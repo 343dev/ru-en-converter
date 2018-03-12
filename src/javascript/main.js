7d9081f8d6901690d25c2332d8aefd882c210aca
343dev <343dev@users.noreply.github.com>
2018-03-12 22:51:30 +0500

    Create ServiceWorker

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,1 +1,13 @@
 import './converter'
+
+/**
+ * ServiceWorker
+ */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('serviceWorker.js', { scope: './' })
+    .then(() => console.log('[ServiceWorker]: Registered'))
+    .catch(error => console.log('[ServiceWorker]: Registration error', error))
+} else {
+  console.log('The current browser doesn\'t support service workers.')
+}
