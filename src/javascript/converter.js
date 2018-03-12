e92ec1c26e60b392128c19fd29f4307e6b54f081
343dev <343dev@users.noreply.github.com>
2018-03-12 22:52:57 +0500

    Rewrite "converter.js"

--- src/javascript/converter.js
+++ src/javascript/converter.js
@@ -1,48 +1,32 @@
-/**
- * Converter
- */
 import ru from 'convert-layout/ru'
 import copy from 'copy-text-to-clipboard'
 
 
+/**
+ * Converter
+ */
 const converter = document.querySelector('.converter')
+const converterInputRU = converter.querySelector('#russian')
+const converterInputEN = converter.querySelector('#english')
+const copyBtnRU = converter.querySelector('#copy-russian')
+const copyBtnEN = converter.querySelector('#copy-english')
 
 /*
   Inputs
  */
-const converterInputRU = converter.querySelector('#russian')
-const converterInputEN = converter.querySelector('#english')
-
-converterInputRU.addEventListener('blur', () => {
-  converterInputRU.value = ru.fromEn(converterInputRU.value)
-  converterInputEN.value = ru.toEn(converterInputRU.value)
-})
-
-converterInputEN.addEventListener('blur', () => {
-  converterInputEN.value = ru.toEn(converterInputEN.value)
-  converterInputRU.value = ru.fromEn(converterInputEN.value)
-})
+converterInputRU.addEventListener('focus', () => { converterInputRU.select() })
+converterInputEN.addEventListener('focus', () => { converterInputEN.select() })
 
 converterInputRU.addEventListener('keyup', () => {
-  converterInputRU.value = ru.fromEn(converterInputRU.value)
   converterInputEN.value = ru.toEn(converterInputRU.value)
 })
 
 converterInputEN.addEventListener('keyup', () => {
-  converterInputEN.value = ru.toEn(converterInputEN.value)
   converterInputRU.value = ru.fromEn(converterInputEN.value)
 })
 
 /*
   Copy buttons
  */
-const copyBtnRU = converter.querySelector('#copy-russian')
-const copyBtnEN = converter.querySelector('#copy-english')
-
-copyBtnRU.addEventListener('click', () => {
-  copy(converterInputRU.value)
-})
-
-copyBtnEN.addEventListener('click', () => {
-  copy(converterInputEN.value)
-})
+copyBtnRU.addEventListener('click', () => copy(converterInputRU.value))
+copyBtnEN.addEventListener('click', () => copy(converterInputEN.value))
